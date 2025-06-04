0fd5828c12cd67fc23a18d89b3c16f023c73e194
北北 <pokemonnick888@gmail.com>
2025-06-04 13:52:16 +0800

    Initial commit

---
+++ src/main/java/name/modid/PathToLegends.java
@@ -0,0 +1,15 @@
+package name.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PathToLegends implements ModInitializer {
+	public static final String MOD_ID = "path-to-legends";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/name/modid/PathToLegendsClient.java
@@ -0,0 +1,11 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PathToLegendsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
